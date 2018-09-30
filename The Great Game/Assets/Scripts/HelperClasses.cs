cdd796b1b300c03c5ce6d5c701da722a98ad13c9
Steve Gotzel <masterredstorm@gmail.com>
2018-09-30 18:03:36 +0200

    Add empty scripts for later completion Add raycast on click from Camera

---
+++ The Great Game/Assets/Scripts/HelperClasses.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+// custom helper class
+public class NullableRaycastHit2D {
+    public bool success = false;
+    public RaycastHit2D hit = new RaycastHit2D();
+}
---
+++ The Great Game/Assets/Scripts/MouseController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+    [Header("Camera Settings")]
+    [Tooltip("Main Camera")]
+    Camera camera;
+
+    // Use this for initialization
+    void Start() {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Pew");
+
+            ////// hierbei gab es das Problem das ich nicht mehr auf die properties zugreifen kann
+            ////// diese lösung gefällt mir aber eindeutig besser:
+            ////RaycastHit2D? hitNullable = CastRay(camera);
+            ////RaycastHit2D hit = hitNullable ?? new RaycastHit2D();   // ! is there a better way to use the properties of a nullable variable? 
+
+            NullableRaycastHit2D hit = CastRay(camera);
+
+            if (hit.success == true) {
+                // test
+                Debug.Log("Hit object: " + hit.hit.collider.name);
+
+                // Move player to hit Object/Position
+
+                // Stop focusing any objects
+            }
+        }
+    }
+
+    // Helper Functions
+    public NullableRaycastHit2D CastRay(Camera camera) {
+        NullableRaycastHit2D returnvalue = new NullableRaycastHit2D();
+        Vector3 ray = camera.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray, Vector3.back);
+        if (hit) {
+            returnvalue.success = true;
+            returnvalue.hit = hit;
+        }
+        return returnvalue;
+    }
+}
